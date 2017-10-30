55728d6ce3f2017fdbaa46f36d0e4d3168e9698d
Andrei Cristian Petcu <andrei@ceata.org>
2017-10-30 06:53:50 +0200

    async await and icons

--- background.js
+++ background.js
@@ -1,25 +1,12 @@
 
-function handleUpdated(tabId, changeInfo, tabInfo) {
-  function logTabsForWindows(windowInfoArray) {
-    browser.tabs.query({currentWindow: true, active: true})
-    .then(activeTab => {
-      if(activeTab[0].url === "about:newtab"){
-        var updating = browser.windows.update(
-          activeTab[0].windowId, { titlePreface: "about:newtab" }
-        )
-      } else {
-        var updating = browser.windows.update(
-          activeTab[0].windowId, { titlePreface: " " }
-        )
-      }
-    });
+async function handleUpdated(tabId, changeInfo, tabInfo) {
+  const activeTabs = await browser.tabs.query({ currentWindow: true, active: true })
+  const activeTab = activeTabs[0];
+  var newTitlePreface = " ";
+  if (activeTab.url === "about:newtab") {
+    newTitlePreface = "about:newtab ";
   }
-  
-  var getting = browser.windows.getAll({
-    populate: true,
-    windowTypes: ["normal"]
-  });
-  getting.then(logTabsForWindows);
+  browser.windows.update(activeTab.windowId, { titlePreface: newTitlePreface });
 }
 
 browser.tabs.onUpdated.addListener(handleUpdated);
